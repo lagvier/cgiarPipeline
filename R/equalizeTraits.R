2cb29a3befecae0ac3b2c40d9c442529957e80d3
covaruber <cova_ruber@live.com.mx>
2024-03-27 08:46:53 +0800

    equalize trait function adjusted to deal with weirdNames

--- R/equalizeTraits.R
+++ R/equalizeTraits.R
@@ -1,7 +1,10 @@
 equalizeTraits <- function(object, traits, newName=NULL){
   mydata <- object$data$pheno
   newValue <- apply(mydata[,traits, drop=FALSE],1, function(x){mean(x, na.rm=TRUE)})
-  if(is.null(newName)){
+  if(is.null(newName) ){
+    newName <- paste(traits,collapse = "_")
+  }
+  if( newName == "" ){
     newName <- paste(traits,collapse = "_")
   }
   mydata[,newName] <- newValue
